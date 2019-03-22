61fc29287811d11b79d526fa03553e3a52f4a9d8
rickard uttermalm <rickard.uttermalm@gmail.com>
2019-03-22 14:29:37 +0100

    set up first components

---
+++ src/Components/Search.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class Search extends Component
+{
+    render()
+    {
+        return(
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-sm-0 col-md-6"></div>
+                <div className="col-sm-12 col-md-6"></div>      
+            </div> 
+       </div>
+        );
+    }
+}
